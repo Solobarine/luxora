2859bba023f6e8c2f668a99149a6e7540eff492d
Solly <solobarine@gmail.com>
2024-09-26 13:38:07 +0100

    Add Build folder

---
+++ public/build/assets/ApplicationLogo-Cz6wdwTY.js
@@ -0,0 +1,1 @@
+import{j as i}from"./app-CdyAb62z.js";function t(o){return i.jsx("img",{src:"/logo.avif",...o})}export{t as A};
---
+++ public/build/assets/product-Bp7SpGmX.js
@@ -0,0 +1,1 @@
+const c=s=>{const n={};return s.forEach(e=>{Object.keys(n).includes(e.name)?n[e.name]=[...n[e.name],e]:n[e.name]=[e]}),n};export{c as g};
